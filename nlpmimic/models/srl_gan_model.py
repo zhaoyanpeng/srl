bug fixed: ensure the positiveness of the softmax temperature and allow fixed softmax temperature; add another debug option `only_reconstruction`

nlpmimic/models/srl_gan_model.py
@@ -28,6 +28,7 @@ class GanSemanticRoleLabeler(Model):
                  srl_encoder: Seq2VecEncoder,
                  binary_feature_dim: int,
                  temperature: float = 1.0,
+                 fixed_temperature: bool = False,
                  embedding_dropout: float = 0.0,
                  initializer: InitializerApplicator = InitializerApplicator(),
                  regularizer: Optional[RegularizerApplicator] = None,
@@ -36,6 +37,7 @@ class GanSemanticRoleLabeler(Model):
         super(GanSemanticRoleLabeler, self).__init__(vocab, regularizer)
 
         self.minimum = 1e-20
+        self.minimum_temperature = 0.001
 
         self.token_embedder = token_embedder
         self.lemma_embedder = lemma_embedder
@@ -50,7 +52,7 @@ class GanSemanticRoleLabeler(Model):
 
         self.seq_encoder = seq_encoder
         self.srl_encoder = srl_encoder
-        self.temperature = torch.nn.Parameter(torch.tensor(temperature)) 
+        self.temperature = torch.nn.Parameter(torch.tensor(temperature), requires_grad=fixed_temperature) 
 
         # There are exactly 2 binary features for the verb predicate embedding.
         self.binary_feature_embedding = Embedding(2, binary_feature_dim)
@@ -73,6 +75,7 @@ class GanSemanticRoleLabeler(Model):
                 predicates: torch.LongTensor,
                 retrive_generator_loss: bool = False,
                 reconstruction_loss: bool = False,
+                only_reconstruction: bool = False,
                 srl_frames: torch.LongTensor = None,
                 metadata: List[Dict[str, Any]] = None) -> Dict[str, torch.Tensor]:
         # pylint: disable=arguments-differ
@@ -180,7 +183,7 @@ class GanSemanticRoleLabeler(Model):
                               'n_embedded_predicates': embedded_noun_predicates,
                               'n_embedded_labels': embedded_noun_labels}
 
-            self._gan_loss(gan_loss_input, full_label_masks, output_dict, retrive_generator_loss)
+            self._gan_loss(gan_loss_input, full_label_masks, output_dict, retrive_generator_loss, only_reconstruction)
             
             #print('\nlogits:\n{}'.format(logits))
             #print('\ngold_labels:\n{}'.format(srl_frames[batch_size:, :]))
@@ -275,7 +278,16 @@ class GanSemanticRoleLabeler(Model):
                   input_dict: Dict[str, torch.Tensor], 
                   mask: torch.Tensor, 
                   output_dict: Dict[str, Any],
-                  retrive_generator_loss: bool) -> None:
+                  retrive_generator_loss: bool,
+                  return_without_computation: bool = False) -> None:
+        if return_without_computation:
+            default_value = torch.tensor(0.0, dtype=torch.float, device=mask.device)
+            if retrive_generator_loss:
+                output_dict['gen_loss'] = default_value 
+            else:
+                output_dict['dis_loss'] = default_value
+            return None
+
         embedded_noun_tokens = input_dict['n_embedded_tokens']
         embedded_noun_lemmas = input_dict['n_embedded_lemmas']
         embedded_noun_predicates = input_dict['n_embedded_predicates']
@@ -335,6 +347,9 @@ class GanSemanticRoleLabeler(Model):
             logits.unsqueeze(0)
         
         batch_size, sequence_length, _ = logits.size()
+
+        self.temperature.data.clamp_(min = self.minimum_temperature)
+
         class_probs = F.gumbel_softmax(
             torch.log(logits.view(-1, self.num_classes) + self.minimum), 
             tau = self.temperature,

nlpmimic/training/srl_gan_trainer.py
@@ -160,7 +160,8 @@ class GanSrlTrainer(Trainer):
                    batch: torch.Tensor, 
                    for_training: bool, 
                    retrive_generator_loss: bool,
-                   reconstruction_loss: bool = True) -> torch.Tensor:
+                   reconstruction_loss: bool = True,
+                   only_reconstruction: bool = False) -> torch.Tensor:
         """
         Does a forward pass on the given batch and returns the ``loss`` value in the result.
         If ``for_training`` is `True` also applies regularization penalty.
@@ -172,7 +173,8 @@ class GanSrlTrainer(Trainer):
             batch = nn_util.move_to_device(batch, self._cuda_devices[0])
             output_dict = self.model(**batch, 
                                      retrive_generator_loss=retrive_generator_loss, 
-                                     reconstruction_loss=reconstruction_loss)
+                                     reconstruction_loss=reconstruction_loss,
+                                     only_reconstruction=only_reconstruction)
         try:
             if for_training: # compulsory loss
                 if retrive_generator_loss:
@@ -276,18 +278,19 @@ class GanSrlTrainer(Trainer):
             batch_num_total = self._batch_num_total
             
             #print(batch)
-            print('----------------------0. model.temperature is {}'.format(self.model.temperature.item()))
-
+            
             # update generator
             self.optimizer.zero_grad()
             gen_loss, rec_loss = self.batch_loss(batch, 
                                       for_training=True, 
                                       retrive_generator_loss=True,
-                                      reconstruction_loss=True)
+                                      reconstruction_loss=True,
+                                      only_reconstruction=True)
             if torch.isnan(gen_loss):
                 raise ValueError("nan loss encountered")
             if torch.isnan(rec_loss):
                 raise ValueError("nan loss encountered")
+            print('----------------------0. model.temperature is {}'.format(self.model.temperature.item()))
             
             # different methods of pre-training the generator, this will be switched 
             # to the unsupervised training as soon as we start training the discriminator
@@ -310,7 +313,7 @@ class GanSrlTrainer(Trainer):
             #logger.info('------------------------optimizing the generator')
             print('----------------------1. model.temperature is {}'.format(self.model.temperature.item()))
            
-            if epoch >= self.dis_skip_nepoch or g_loss <= 0.1: # do optimize the discriminator
+            if epoch >= self.dis_skip_nepoch and g_loss <= 0.3: # do optimize the discriminator
                 # reset the training of the generator to the unsupervised training
                 self.gen_pretraining = -1
 