zhaoyanpeng/srl

commit a0a310d68a20bf2f865ad405eed3ee00b1b5670c

improve generator: add a new option `zero_null_lemma_embedding`, reset the embedding of the null lemma to the vector of all zeros; run `srl_gan_toy.sh` for details; debugging prints are not removed

mimiconf/srl_gan_toy.jsonnet
@@ -38,7 +38,7 @@
             "embedding_dim": 2,
             //"pretrained_file": "/disk/scratch1/s1847450/data/embeding/glove.6B.100d.vec.c2009",
             "vocab_namespace": "lemmas",
-            "trainable": true 
+            "trainable": false 
         }
       }
     },
@@ -72,8 +72,8 @@
         "tag_projection_layer.*weight",
         {
           //"type": "orthogonal"
-          //"type": "xavier_uniform"
-          "type": "normal"
+          "type": "xavier_uniform"
+          //"type": "normal"
           //"type": "uniform"
           //"type": "xavier_normal"
         }
@@ -93,8 +93,9 @@
     ],
     "binary_feature_dim": 2, 
     "temperature": 0.01,
-    "fixed_temperature": true,
-    "mask_empty_labels": false 
+    "fixed_temperature": false,
+    "mask_empty_labels": false,
+    "zero_null_lemma_embedding": true,
   },
   "iterator": {
     "type": "bucket",
@@ -108,7 +109,7 @@
     "patience": 20,
     "shuffle": false,
     "validation_metric": "+f1-measure-overall",
-    "cuda_device": 0,
+    "cuda_device": 3,
     "dis_skip_nepoch": 5,
     "gen_pretraining": 0,  
     "dis_loss_scalar": 0.01,

nlpmimic/models/srl_gan_model.py
@@ -27,10 +27,11 @@ class GanSemanticRoleLabeler(Model):
                  seq_encoder: Seq2SeqEncoder,
                  srl_encoder: Seq2VecEncoder,
                  binary_feature_dim: int,
-                 temperature: float = 1.0,
+                 temperature: float = 1.,
                  fixed_temperature: bool = False,
                  mask_empty_labels: bool = True,
-                 embedding_dropout: float = 0.0,
+                 embedding_dropout: float = 0.,
+                 zero_null_lemma_embedding: bool = False, 
                  initializer: InitializerApplicator = InitializerApplicator(),
                  regularizer: Optional[RegularizerApplicator] = None,
                  label_smoothing: float = None,
@@ -48,6 +49,20 @@ class GanSemanticRoleLabeler(Model):
         
         self.num_classes = self.vocab.get_vocab_size("srl_tags")
         self.null_lemma_idx = self.vocab.get_token_index("NULL_LEMMA", namespace="lemmas")
+        
+        if zero_null_lemma_embedding:
+            embedder = getattr(self.lemma_embedder, 'token_embedder_{}'.format('lemmas'))
+            print(embedder.weight) 
+            print(embedder.weight.size())
+            embedder.weight[self.null_lemma_idx, :] = 0.
+            print(embedder.weight)
+        
+            a = {'lemmas': torch.tensor(self.null_lemma_idx)}
+            vec = self.lemma_embedder(a)
+            print(vec)
+            vec = self.lemma_embedder(a)
+            print(vec)
+
         
         # For the span based evaluation, we don't want to consider labels
         # for verb, because the verb index is provided to the model.
@@ -156,16 +171,16 @@ class GanSemanticRoleLabeler(Model):
         
         
         if self.training:
-            noun_labels, embedded_noun_labels = self._argmax_logits(class_probabilities, used_mask)
+            noun_labels, _ = self._argmax_logits(class_probabilities, used_mask)
             #noun_labels = self._logits_to_index(class_probabilities, used_mask) 
 
             predicted_labels = torch.cat([srl_frames[:batch_size, :], noun_labels], 0)
             full_label_masks = mask.clone() # srl label masks
-            """            
+                        
             print('\nlemmas: ', lemmas)
             print('predicted_labels: ', noun_labels, noun_labels.requires_grad)
             print('srl_frames: ', srl_frames, srl_frames.requires_grad)
-            """
+
             if self.mask_empty_labels:
                 # mask out empty labels 
                 full_label_masks[predicted_labels == 0] = 0
@@ -175,18 +190,21 @@ class GanSemanticRoleLabeler(Model):
                 lemmas = lemmas * (predicted_labels != 0).long()
                 lemmas = lemmas + (predicted_labels == 0).long() * self.null_lemma_idx
                 lemmas = {'lemmas': lemmas}
-            """
+            
             print('masked lemmas:', lemmas)
             print('label_masks ', full_label_masks, full_label_masks.requires_grad)
-            import sys
-            sys.exit(0)
-            """
+            
             embedded_lemma_input = self.embedding_dropout(self.lemma_embedder(lemmas))
             embedded_verb_lemmas = embedded_lemma_input[:batch_size, :] 
             embedded_noun_lemmas = embedded_lemma_input[batch_size:, :] 
                 
-            #embedded_noun_labels = self.embedding_dropout(self.label_embedder(noun_labels))
+            embedded_noun_labels = self.embedding_dropout(self.label_embedder(noun_labels))
             embedded_verb_labels = self.embedding_dropout(self.label_embedder(srl_frames[:batch_size, :]))
+            
+            print(embedded_noun_lemmas)
+
+            import sys
+            sys.exit(0)
             
             gan_loss_input = {'v_embedded_tokens': embedded_verb_tokens,
                               'v_embedded_lemmas': embedded_verb_lemmas,
@@ -373,6 +391,16 @@ class GanSemanticRoleLabeler(Model):
         #print('\ngumbel_size: {}\n'.format(class_probs.size()))
         
         #print(self.label_embedder.weight)
+        
+        print()
+        _, max_likelihood_sequence = torch.max(logits, -1)
+        for i, length in enumerate(sequence_lengths):
+            max_likelihood_sequence[i, length:] = 0
+            tags = [self.vocab.get_token_from_index(x.item(), namespace="srl_tags")
+                    for x in max_likelihood_sequence[i, :]]
+            print(tags)
+        print('\nmax_ll_sequence:\n{}'.format(max_likelihood_sequence))
+         
         
         embedded_labels = torch.matmul(class_probs, self.label_embedder.weight) 
         _, max_likelihood_sequence = torch.max(class_probs, -1)

commit 8bc7a5888f47f1305d36c0ca8f11e52e1a2bb1e3

bug commit: this commit records a bug introduced by the commit `a0a310d68a20bf2f865ad405eed3ee00b1b5670c`; line 169 & 196 in `srl_gan_model.py` hinder gradients from back-propagating to tunable parameters such as self.temperature which are used in the Straight-Through Gumbel Softmax; it is because the operation of looking up embedding table is not differential; the correct way is multiplying one-hot vectors output from the ST-GS by the srl label embedding matrix; however, this results in a `UnsafeViewBackward` gradient function. What does this mean? How will it influence gradient back-propagation? Need to figure it out; For now, this is the only way I can think of, which still makes gradient back-propagation possible

executor/srl_gan.sh
@@ -7,7 +7,7 @@ proot="/afs/inf.ed.ac.uk/user/s18/s1847450/Code/nlpmimic"
 droot="/disk/scratch1/s1847450"
 param_path="$proot/mimiconf"
 
-model_name="srl_gan_fixed_lemma_mask_1.005"
+model_name="srl_gan_1.05_t10_dropout_tune"
 param_name="srl_gan.jsonnet"
 model_path=$droot/model
 

mimiconf/srl_gan.jsonnet
@@ -22,7 +22,8 @@
   //"train_dy_path": "/disk/scratch1/s1847450/data/conll09/separated/train.verb",
   "train_dx_path": "/disk/scratch1/s1847450/data/conll09/separated/noun.picked",
   "train_dy_path": "/disk/scratch1/s1847450/data/conll09/separated/verb.picked",
-  "validation_data_path": "/disk/scratch1/s1847450/data/conll09/separated/devel.noun",
+  //"validation_data_path": "/disk/scratch1/s1847450/data/conll09/separated/devel.noun",
+  "validation_data_path": "/disk/scratch1/s1847450/data/conll09/devel.small.noun",
   "vocab_src_path": "/disk/scratch1/s1847450/data/conll09/separated/vocab.src",
   "datasets_for_vocab_creation": ["vocab"],
   "model": {
@@ -87,14 +88,15 @@
       [
         "tag_projection_layer.*weight",
         {
-          "type": "xavier_uniform"
+            "type": "orthogonal"
         }
       ]
     ],
     "binary_feature_dim": 100, 
     "temperature": 1e-2,
-    "fixed_temperature": false,
-    "mask_empty_labels": true, 
+    "fixed_temperature": true,
+    "mask_empty_labels": false, 
+    "embedding_dropout": 0.3,
     "zero_null_lemma_embedding": true,
   },
   "iterator": {
@@ -111,10 +113,10 @@
     "shuffle": true,
     "num_serialized_models_to_keep": 10,
     "validation_metric": "+f1-measure-overall",
-    "cuda_device": 0,
+    "cuda_device": 2,
     "dis_skip_nepoch": 0,
     "gen_pretraining": -1,  
-    "dis_loss_scalar": 0.005,
+    "dis_loss_scalar": 1.0,
     "gen_loss_scalar": 1.0,
     "optimizer": {
       "type": "adadelta",

mimiconf/srl_gan_toy.jsonnet
@@ -15,7 +15,7 @@
   
   "train_dx_path": "/disk/scratch1/s1847450/data/conll09/bitgan/noun.bit",
   "train_dy_path": "/disk/scratch1/s1847450/data/conll09/bitgan/verb.bit",
-  "validation_data_path": "/disk/scratch1/s1847450/data/conll09/bitgan/noun.bit",
+  //"validation_data_path": "/disk/scratch1/s1847450/data/conll09/devel.small.noun",
   //"vocab_src_path": "/disk/scratch1/s1847450/data/conll09/separated/vocab.src",
   //"datasets_for_vocab_creation": ["vocab"],
   "model": {
@@ -71,8 +71,8 @@
       [
         "tag_projection_layer.*weight",
         {
-          //"type": "orthogonal"
-          "type": "xavier_uniform"
+          "type": "orthogonal"
+          //"type": "xavier_uniform"
           //"type": "normal"
           //"type": "uniform"
           //"type": "xavier_normal"
@@ -100,7 +100,7 @@
   "iterator": {
     "type": "bucket",
     "sorting_keys": [["tokens", "num_tokens"]],
-    "batch_size": 5 
+    "batch_size": 2 
   },
   "trainer": {
     "type": "srl_gan",
@@ -110,8 +110,8 @@
     "shuffle": false,
     "validation_metric": "+f1-measure-overall",
     "cuda_device": 3,
-    "dis_skip_nepoch": 5,
-    "gen_pretraining": 0,  
+    "dis_skip_nepoch": 0,
+    "gen_pretraining": -1,  
     "dis_loss_scalar": 0.01,
     "gen_loss_scalar": 5.0,
     "optimizer": {

nlpmimic/models/srl_gan_model.py
@@ -209,7 +209,6 @@ class GanSemanticRoleLabeler(Model):
                               'n_embedded_lemmas': embedded_noun_lemmas,
                               'n_embedded_predicates': embedded_noun_predicates,
                               'n_embedded_labels': embedded_noun_labels}
-
             self._gan_loss(gan_loss_input, full_label_masks, output_dict, retrive_generator_loss, only_reconstruction)
             
             #print('\nlogits:\n{}'.format(logits))
@@ -337,7 +336,6 @@ class GanSemanticRoleLabeler(Model):
         if retrive_generator_loss:
             mask = mask[batch_size:]
             logits = self.srl_encoder(embedded_noun, mask)  
-            logits = logits.squeeze(-1)
             logits = torch.sigmoid(logits)
             # fake labels 
             real_labels = mask[:, 0].detach().clone().fill_(1).float()
@@ -357,7 +355,6 @@ class GanSemanticRoleLabeler(Model):
             embedded_input = torch.cat([embedded_verb, embedded_noun], 0)
             
             logits = self.srl_encoder(embedded_input, mask)
-            logits = logits.squeeze(-1)
             logits = torch.sigmoid(logits)
             # fake labels
             fake_labels = mask[:batch_size, 0].detach().clone().fill_(0).float()
@@ -375,7 +372,7 @@ class GanSemanticRoleLabeler(Model):
         
         batch_size, sequence_length, _ = logits.size()
 
-        self.temperature.data.clamp_(min = self.minimum_temperature)
+        #self.temperature.data.clamp_(min = self.minimum_temperature)
 
         class_probs = F.gumbel_softmax(
             torch.log(logits.view(-1, self.num_classes) + self.minimum), 

nlpmimic/modules/seq2vec_encoders/srl_gan_discriminator.py
@@ -69,6 +69,16 @@ class GanSrlDiscriminator(Seq2VecEncoder):
         projected_tokens = self._projection(tokens)
         projected_tokens = self._activation(projected_tokens)
         
+        # (batch_size, length, dim) -> (batch_size, length, 1)
+        all_logits = self._logits(projected_tokens)
+        all_logits = self._activation(all_logits)
+
+        divider = 1. / torch.sum(mask, -1).float()
+        divider = divider.unsqueeze(-1).unsqueeze(-1).expand(-1, length, -1)
+        # -- * (batch_size, length, 1) -> (batch_size, 1, 1) -> (batch_size,)
+        logits = torch.bmm(all_logits.transpose(1, 2), divider).squeeze(-1).squeeze(-1)
+
+        """
         divider = 1. / torch.sum(mask, -1).float()
         divider = divider.unsqueeze(-1).unsqueeze(-1).expand(-1, length, -1)
         
@@ -76,6 +86,6 @@ class GanSrlDiscriminator(Seq2VecEncoder):
         features = torch.bmm(projected_tokens.transpose(1, 2), divider)
         features = features.squeeze(-1)
 
-        logits = self._logits(features)
-        
+        logits = self._logits(features).squeeze(-1)
+        """
         return logits 

nlpmimic/training/srl_gan_trainer.py
@@ -161,7 +161,8 @@ class GanSrlTrainer(Trainer):
                    for_training: bool, 
                    retrive_generator_loss: bool,
                    reconstruction_loss: bool = True,
-                   only_reconstruction: bool = False) -> torch.Tensor:
+                   only_reconstruction: bool = False,
+                   peep_prediction: bool = False) -> torch.Tensor:
         """
         Does a forward pass on the given batch and returns the ``loss`` value in the result.
         If ``for_training`` is `True` also applies regularization penalty.
@@ -188,6 +189,12 @@ class GanSrlTrainer(Trainer):
                 rec_loss = output_dict["rec_loss"]
             else:
                 rec_loss = None
+            if peep_prediction: #and not for_training:
+                output_dict = self.model.decode(output_dict)
+                tokens = output_dict['tokens'][:10]
+                labels = output_dict['srl_tags'][:10]
+                for token, label in zip(tokens, labels):
+                    print('\n{}\n{}'.format(token, label))
         except KeyError:
             if for_training:
                 raise RuntimeError("The model you are trying to optimize does not contain a"
@@ -273,6 +280,12 @@ class GanSrlTrainer(Trainer):
         
         cumulative_batch_size = 0
         for batch in train_generator_tqdm:
+            if batches_this_epoch % 100 == 0:
+                peep = False 
+                #print('\n----------------------0. model.temperature is {}'.format(self.model.temperature.item()))
+            else:
+                #print('\n----------------------0. model.temperature is {}'.format(self.model.temperature.item()))
+                peep = False
             batches_this_epoch += 1
             self._batch_num_total += 1
             batch_num_total = self._batch_num_total
@@ -285,12 +298,13 @@ class GanSrlTrainer(Trainer):
                                       for_training=True, 
                                       retrive_generator_loss=True,
                                       reconstruction_loss=True,
-                                      only_reconstruction=False)
+                                      only_reconstruction=False,
+                                      peep_prediction=peep)
             if torch.isnan(gen_loss):
                 raise ValueError("nan loss encountered")
             if torch.isnan(rec_loss):
                 raise ValueError("nan loss encountered")
-            #print('----------------------0. model.temperature is {}'.format(self.model.temperature.item()))
+            print('----------------------0. model.temperature is {}'.format(self.model.temperature.item()))
             
             # different methods of pre-training the generator, this will be switched 
             # to the unsupervised training as soon as we start training the discriminator
@@ -311,7 +325,7 @@ class GanSrlTrainer(Trainer):
             train_loss += gen_loss.item()
             #logger.info('')
             #logger.info('------------------------optimizing the generator')
-            #print('----------------------1. model.temperature is {}'.format(self.model.temperature.item()))
+            print('----------------------1. model.temperature is {}'.format(self.model.temperature.item()))
            
             if epoch >= self.dis_skip_nepoch: #and g_loss <= 0.3: # do optimize the discriminator
                 # reset the training of the generator to the unsupervised training
@@ -347,7 +361,7 @@ class GanSrlTrainer(Trainer):
             #    sys.exit(0)
 
             reconstruction_loss += rec_loss.item() 
-            #print('----------------------2. model.temperature is {}'.format(self.model.temperature.item()))
+            print('----------------------2. model.temperature is {}'.format(self.model.temperature.item()))
 
             # Update the description with the latest metrics
             metrics = mimic_training_util.get_metrics(self.model, 
@@ -427,7 +441,8 @@ class GanSrlTrainer(Trainer):
             _, loss = self.batch_loss(batch, 
                                       for_training=False,
                                       retrive_generator_loss=False, # dead; active only during training
-                                      reconstruction_loss=True) # implying gold labels exist
+                                      reconstruction_loss=True, # implying gold labels exist
+                                      peep_prediction=True)
             if loss is not None:
                 # You shouldn't necessarily have to compute a loss for validation, so we allow for
                 # `loss` to be None.  We need to be careful, though - `batches_this_epoch` is
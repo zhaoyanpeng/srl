bug commit: this commit records a bug introduced by the commit `a0a310d68a20bf2f865ad405eed3ee00b1b5670c`; line 169 & 196 in `srl_gan_model.py` hinder gradients from back-propagating to tunable parameters such as self.temperature which are used in the Straight-Through Gumbel Softmax; it is because the operation of looking up embedding table is not differential; the correct way is multiplying one-hot vectors output from the ST-GS by the srl label embedding matrix; however, this results in a `UnsafeViewBackward` gradient function. What does this mean? How will it influence gradient back-propagation? Need to figure it out; For now, this is the only way I can think of, which still makes gradient back-propagation possible

nlpmimic/models/srl_gan_model.py
@@ -209,7 +209,6 @@ class GanSemanticRoleLabeler(Model):
                               'n_embedded_lemmas': embedded_noun_lemmas,
                               'n_embedded_predicates': embedded_noun_predicates,
                               'n_embedded_labels': embedded_noun_labels}
-
             self._gan_loss(gan_loss_input, full_label_masks, output_dict, retrive_generator_loss, only_reconstruction)
             
             #print('\nlogits:\n{}'.format(logits))
@@ -337,7 +336,6 @@ class GanSemanticRoleLabeler(Model):
         if retrive_generator_loss:
             mask = mask[batch_size:]
             logits = self.srl_encoder(embedded_noun, mask)  
-            logits = logits.squeeze(-1)
             logits = torch.sigmoid(logits)
             # fake labels 
             real_labels = mask[:, 0].detach().clone().fill_(1).float()
@@ -357,7 +355,6 @@ class GanSemanticRoleLabeler(Model):
             embedded_input = torch.cat([embedded_verb, embedded_noun], 0)
             
             logits = self.srl_encoder(embedded_input, mask)
-            logits = logits.squeeze(-1)
             logits = torch.sigmoid(logits)
             # fake labels
             fake_labels = mask[:batch_size, 0].detach().clone().fill_(0).float()
@@ -375,7 +372,7 @@ class GanSemanticRoleLabeler(Model):
         
         batch_size, sequence_length, _ = logits.size()
 
-        self.temperature.data.clamp_(min = self.minimum_temperature)
+        #self.temperature.data.clamp_(min = self.minimum_temperature)
 
         class_probs = F.gumbel_softmax(
             torch.log(logits.view(-1, self.num_classes) + self.minimum), 

nlpmimic/modules/seq2vec_encoders/srl_gan_discriminator.py
@@ -69,6 +69,16 @@ class GanSrlDiscriminator(Seq2VecEncoder):
         projected_tokens = self._projection(tokens)
         projected_tokens = self._activation(projected_tokens)
         
+        # (batch_size, length, dim) -> (batch_size, length, 1)
+        all_logits = self._logits(projected_tokens)
+        all_logits = self._activation(all_logits)
+
+        divider = 1. / torch.sum(mask, -1).float()
+        divider = divider.unsqueeze(-1).unsqueeze(-1).expand(-1, length, -1)
+        # -- * (batch_size, length, 1) -> (batch_size, 1, 1) -> (batch_size,)
+        logits = torch.bmm(all_logits.transpose(1, 2), divider).squeeze(-1).squeeze(-1)
+
+        """
         divider = 1. / torch.sum(mask, -1).float()
         divider = divider.unsqueeze(-1).unsqueeze(-1).expand(-1, length, -1)
         
@@ -76,6 +86,6 @@ class GanSrlDiscriminator(Seq2VecEncoder):
         features = torch.bmm(projected_tokens.transpose(1, 2), divider)
         features = features.squeeze(-1)
 
-        logits = self._logits(features)
-        
+        logits = self._logits(features).squeeze(-1)
+        """
         return logits 

nlpmimic/training/srl_gan_trainer.py
@@ -161,7 +161,8 @@ class GanSrlTrainer(Trainer):
                    for_training: bool, 
                    retrive_generator_loss: bool,
                    reconstruction_loss: bool = True,
-                   only_reconstruction: bool = False) -> torch.Tensor:
+                   only_reconstruction: bool = False,
+                   peep_prediction: bool = False) -> torch.Tensor:
         """
         Does a forward pass on the given batch and returns the ``loss`` value in the result.
         If ``for_training`` is `True` also applies regularization penalty.
@@ -188,6 +189,12 @@ class GanSrlTrainer(Trainer):
                 rec_loss = output_dict["rec_loss"]
             else:
                 rec_loss = None
+            if peep_prediction: #and not for_training:
+                output_dict = self.model.decode(output_dict)
+                tokens = output_dict['tokens'][:10]
+                labels = output_dict['srl_tags'][:10]
+                for token, label in zip(tokens, labels):
+                    print('\n{}\n{}'.format(token, label))
         except KeyError:
             if for_training:
                 raise RuntimeError("The model you are trying to optimize does not contain a"
@@ -273,6 +280,12 @@ class GanSrlTrainer(Trainer):
         
         cumulative_batch_size = 0
         for batch in train_generator_tqdm:
+            if batches_this_epoch % 100 == 0:
+                peep = False 
+                #print('\n----------------------0. model.temperature is {}'.format(self.model.temperature.item()))
+            else:
+                #print('\n----------------------0. model.temperature is {}'.format(self.model.temperature.item()))
+                peep = False
             batches_this_epoch += 1
             self._batch_num_total += 1
             batch_num_total = self._batch_num_total
@@ -285,12 +298,13 @@ class GanSrlTrainer(Trainer):
                                       for_training=True, 
                                       retrive_generator_loss=True,
                                       reconstruction_loss=True,
-                                      only_reconstruction=False)
+                                      only_reconstruction=False,
+                                      peep_prediction=peep)
             if torch.isnan(gen_loss):
                 raise ValueError("nan loss encountered")
             if torch.isnan(rec_loss):
                 raise ValueError("nan loss encountered")
-            #print('----------------------0. model.temperature is {}'.format(self.model.temperature.item()))
+            print('----------------------0. model.temperature is {}'.format(self.model.temperature.item()))
             
             # different methods of pre-training the generator, this will be switched 
             # to the unsupervised training as soon as we start training the discriminator
@@ -311,7 +325,7 @@ class GanSrlTrainer(Trainer):
             train_loss += gen_loss.item()
             #logger.info('')
             #logger.info('------------------------optimizing the generator')
-            #print('----------------------1. model.temperature is {}'.format(self.model.temperature.item()))
+            print('----------------------1. model.temperature is {}'.format(self.model.temperature.item()))
            
             if epoch >= self.dis_skip_nepoch: #and g_loss <= 0.3: # do optimize the discriminator
                 # reset the training of the generator to the unsupervised training
@@ -347,7 +361,7 @@ class GanSrlTrainer(Trainer):
             #    sys.exit(0)
 
             reconstruction_loss += rec_loss.item() 
-            #print('----------------------2. model.temperature is {}'.format(self.model.temperature.item()))
+            print('----------------------2. model.temperature is {}'.format(self.model.temperature.item()))
 
             # Update the description with the latest metrics
             metrics = mimic_training_util.get_metrics(self.model, 
@@ -427,7 +441,8 @@ class GanSrlTrainer(Trainer):
             _, loss = self.batch_loss(batch, 
                                       for_training=False,
                                       retrive_generator_loss=False, # dead; active only during training
-                                      reconstruction_loss=True) # implying gold labels exist
+                                      reconstruction_loss=True, # implying gold labels exist
+                                      peep_prediction=True)
             if loss is not None:
                 # You shouldn't necessarily have to compute a loss for validation, so we allow for
                 # `loss` to be None.  We need to be careful, though - `batches_this_epoch` is